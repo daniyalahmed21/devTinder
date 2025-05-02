e2ba8cc2cae794b853017fa7796107f471a37d92
Daniyal Ahmed <da9033431@gmail.com>
2025-05-02 15:41:11 +0500

    user schema

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
+const { connectDb } = require("./config/database");
+
+connectDb()
+  .then(() => {
+    console.log("connection established successfully");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening at port 3000");
+    });
+  })
+  .catch(() => {
+    console.error("Database cannot be connected!");
+  });
 
 // Normal route
 app.get("/", (req, res) => {
-  res.send("mnbv");
+  res.send("home");
 });
 
 app.use("/admin", adminAuth);
@@ -12,7 +24,7 @@ app.use("/admin", adminAuth);
 app.use("/user", userAuth);
 
 app.get("/user/login", (req, res) => {
-  throw new Error("something went wrong"); // will trigger error handler
+  // throw new Error("something went wrong"); // will trigger error handler
   console.log("user logged in");
   res.send("login");
 });
@@ -28,8 +40,4 @@ app.get("/admin/getAllData", adminAuth, (req, res) => {
 // Error handling middleware — must be last
 app.use((err, req, res, next) => {
   res.status(500).send("something went wrong");
-});
-
-app.listen(3000, () => {
-  console.log("Server is successfully listening at port 3000");
 });
