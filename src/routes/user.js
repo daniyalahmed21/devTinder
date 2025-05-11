a35a8a1cdc5a1cfe8aec5bc28cd2716e2b9adadb
Daniyal Ahmed <da9033431@gmail.com>
2025-05-11 17:04:29 +0500

    feed and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const connectionRequest = require("../models/connectionRequest");
+const { User } = require("../models/user");
 
 // get all pending connection req for the logged in user
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
@@ -24,6 +25,7 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
   }
 });
 
+//get all the connections of logged in user
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const userId = req.user._id;
@@ -53,4 +55,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+//get the feed for the user http://localhost:3000/feed?page=1&limit=1
+userRouter.get("/feed", userAuth ,async(req, res)=>{
+  try{
+
+    const page = parseInt(req.query.page) || 1
+    let limit = parseInt(req.query.limit) || 5
+    limit = limit > 30 ? 30 : limit
+
+
+
+    const skipped = ()=>{
+      return (page-1)*limit
+    }
+
+    const userId = req.user._id
+    const connectionRequests = await connectionRequest.find({
+      $or:[{toUserId:userId},{fromUserId:userId}]
+    }).select("toUserId fromUserId")
+
+    const excludeFromFeed = new Set()
+
+    connectionRequests.forEach(user=>{
+      excludeFromFeed.add(user.toUserId.toString())
+      excludeFromFeed.add(user.fromUserId.toString())
+    })
+
+
+    const feedUser = await User.find({
+      _id : { $nin: Array.from(excludeFromFeed) }   //transform set into array and apply not in 
+    }).select("_id firstName lastName").skip(skipped).limit(limit)
+
+    res.send(feedUser)
+
+
+  }catch(err){
+    res.status(500).json({ error: err.message });
+  }
+})
+
 module.exports = { userRouter };
